23c38e1eb9c148607883422f8ddf79c7935b6ad8
seokham <68664399+seokham@users.noreply.github.com>
2021-05-11 06:12:57 +0900

    Delete 04. Else_If.py

--- 04. Else_If.py
+++
@@ -1,9 +0,0 @@
-input = 33
-real_hoppang = 11
-real_h1234 = "ab"
-if real_hoppang == input:
-  print("Hello!, hoppang")
-elif real_h1234 == input:
-  print("Hello!, h1234")
-else:
-  print("Who are you?")
