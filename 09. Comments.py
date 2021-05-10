dd698c7c8eeda0663214a0b906691f8d8c0c2e47
seokham <68664399+seokham@users.noreply.github.com>
2021-05-11 06:25:55 +0900

    Update 09. Comments.py

--- 09. Comments.py
+++ 09. Comments.py
@@ -1,17 +1,17 @@
-# 주석에 준하는 효과이며, ''' 로 묶는 순간 문자열로 인식한다.
+# *주석에 준하는 효과이며, ''' 로 묶는 순간 문자열로 인식한다.
 '''
 조건문 예제
 hoppang
 2015
 '''
 
-# user input password
+#* user input password
 input = 33
 real_hoppang = 11
-#real_h1234 = "ab"
+#* real_h1234 = "ab"
 if real_hoppang == input:
   print("Hello!, hoppang")
-#elif real_h1234 == input:
-#  print("Hello!, h1234")
+#* elif real_h1234 == input:
+#* print("Hello!, h1234")
 else:
   print("Who are you?")
