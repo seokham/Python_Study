48b7bd0aaf06d396145fdeee96bf33d2d45abaca
seokham <68664399+seokham@users.noreply.github.com>
2021-05-14 23:56:44 +0900

    Delete Module_1 directory

--- Module_1/module.py
+++
@@ -1,7 +0,0 @@
-
-#* teacher 라는 module로 부터 a라는 함수만을 import 한다.
-#* a라는 함수의 이름을 z로 바꾸고 싶을 때 as를 쓴다.
-from teacher import a as z
-import student as k
-print(z())
-print(k.a())
--- Module_1/teacher.py
+++
@@ -1,8 +0,0 @@
-
-#* 모듈의 도입
-def a():
-    return 'a'
-def b():
-    return 'b'
-def c():
-    return 'c'
